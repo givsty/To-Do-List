87724f4a6075d4a046f2c3c1f89c26e57341c5d9
nikbritoff <65825157+nikbritoff@users.noreply.github.com>
2024-12-24 01:08:59 +0300

    feat-006: Add styles to filter (#9)

--- src/features/filter/Filter.tsx
+++ src/features/filter/Filter.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite';
 import { TaskPriorityChip } from '../../shared/ui/TaskPriorityChip/TaskPriorityChip';
 import { taskStore } from '../../shared/store';
 import { StyledPriorityLabel } from '../../pages/main/ui/CreateTaskForm/styles';
-import { StyledButton } from './styles';
+import { FilterContainer, StyledButton } from './styles';
 
 export const Filter = observer(() => {
   const handleFilterChange = async (priority: 'low' | 'medium' | 'high') => {
@@ -14,29 +14,27 @@ export const Filter = observer(() => {
   };
 
   return (
-    <div>
+    <FilterContainer>
       <StyledPriorityLabel>Filter by:</StyledPriorityLabel>
-      <div>
-        <TaskPriorityChip
-          variant="radio"
-          value="low"
-          onChange={() => handleFilterChange('low')}
-          isActive={taskStore.activeFilter.value === 'low'}
-        />
-        <TaskPriorityChip
-          variant="radio"
-          value="medium"
-          onChange={() => handleFilterChange('medium')}
-          isActive={taskStore.activeFilter.value === 'medium'}
-        />
-        <TaskPriorityChip
-          variant="radio"
-          value="high"
-          onChange={() => handleFilterChange('high')}
-          isActive={taskStore.activeFilter.value === 'high'}
-        />
-        <StyledButton onClick={handleClearFilter}>Clear</StyledButton>
-      </div>
-    </div>
+      <TaskPriorityChip
+        variant="radio"
+        value="low"
+        onChange={() => handleFilterChange('low')}
+        isActive={taskStore.activeFilter.value === 'low'}
+      />
+      <TaskPriorityChip
+        variant="radio"
+        value="medium"
+        onChange={() => handleFilterChange('medium')}
+        isActive={taskStore.activeFilter.value === 'medium'}
+      />
+      <TaskPriorityChip
+        variant="radio"
+        value="high"
+        onChange={() => handleFilterChange('high')}
+        isActive={taskStore.activeFilter.value === 'high'}
+      />
+      <StyledButton onClick={handleClearFilter}>Clear</StyledButton>
+    </FilterContainer>
   );
 });
